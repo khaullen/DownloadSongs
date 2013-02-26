f44f9a5d11aa788280f87991077a6e18328fb002
Colin Regan <colin@redcup.la>
2013-02-26 18:15:28 -0500

    Added URL escaping to deal with crappy URLs

--- download.rb
+++ download.rb
@@ -19,7 +19,7 @@ def parse_mp3_skull(search_term)
 	array = doc.css('div#song_html').map do |song_element|
 		hash = {}
 		hash[:name] = song_element.css('#right_song div b').first.content.chomp(" mp3")
-		hash[:uri] = song_element.css('#right_song a').first['href']
+		hash[:uri] = URI.escape(song_element.css('#right_song a').first['href'])
 		hash[:search_term] = search_term
 		hash.merge(parse_left_content(song_element.css('div.left').first.content))
 	end
@@ -34,7 +34,6 @@ end
 def download_to_path(songs, path="~/Downloads")
 	song = songs.shift
 	File.open(File.expand_path(song[:name] << ".mp3", path), "wb") do |saved_file|
-  	# the following "open" is provided by open-uri
   	open(song[:uri], 'rb') do |read_file|
   		saved_file.write(read_file.read)
   	end
@@ -42,10 +41,11 @@ def download_to_path(songs, path="~/Downloads")
 end
 
 
+search_string = 'lcd_soundsystem_someone_great'
 
 #puts parse_turntable
-puts parse_mp3_skull('robert_francis_junebug')
-#download_to_path(parse_mp3_skull('robert_francis_junebug'))
+#puts parse_mp3_skull(search_string)
+download_to_path(parse_mp3_skull(search_string))
 
 
 
@@ -54,7 +54,7 @@ puts parse_mp3_skull('robert_francis_junebug')
 # - calculate match fit by excluding extraneous terms in song name (remix)
 # - parse song time and size from left_content
 # - recover from errors and download next song in array
-# - url encoding for bad links
 # - accept argument for download path using ARGV
 # - accept argument for reading list of songs from file
 # - add threads for concurrent downloads?
+# - add songs to iTunes playlist
