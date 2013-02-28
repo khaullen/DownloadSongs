e9dcc3b91e280f988e53dca6d600663765ee582d
Colin Regan <colin@redcup.la>
2013-02-28 13:58:38 -0500

    Added command line options

--- download.rb
+++ download.rb
@@ -2,6 +2,7 @@
 
 require 'nokogiri'
 require 'open-uri'
+require 'optparse'
 
 def parse_turntable(songs=$stdin.readlines)
 	songs.map do |song|
@@ -40,25 +41,75 @@ end
 def download_to_path(songs, path="~/Downloads")
 	song = songs.shift
 	File.open(File.expand_path(song[:name] << ".mp3", path), "wb") do |saved_file|
-  	open(song[:uri], 'rb') do |read_file|
-  		saved_file.write(read_file.read)
-  	end
-  end
+		open(song[:uri], 'rb') do |read_file|
+			saved_file.write(read_file.read)
+		end
+	end
 end
 
 
-search_string = 'lcd_soundsystem_someone_great'
+options = {}
+
+optparse = OptionParser.new do |opts|
+	opts.banner = "Usage: download.rb [options] song1 song2 ..."
+	
+	options[:play] = false
+	opts.on( '-p', '--play', 'Play song after downloading' ) do
+		options[:play] = true
+	end
+	
+	options[:quality] = 0
+	opts.on( '-q', '--quality [KBPS]', Integer, 'Minimum quality mp3 in KBPS' ) do |kbps|
+		options[:quality] = kbps || 160
+	end
+	
+	options[:verbose] = false
+	opts.on( '-v', '--verbose', 'Output more information' ) do
+		options[:verbose] = true
+	end
+	
+	options[:log] = "log.txt"
+	opts.on( '-l', '--logfile FILE', 'Write log to FILE' ) do |file|
+		options[:log] = file
+	end
+	
+	opts.on( '-h', '--help', 'Display help' ) do
+		puts opts
+		exit
+	end
+end
+
+
+optparse.parse!
+
+puts "Verbose mode enabled" if options[:verbose]
+puts "Logging output to #{options[:log]}"
+puts "Minimum quality: #{options[:quality]}kbps" if options[:quality] > 0
+
+ARGV.each do |song|
+	puts song
+end
+	
+	
 
 #puts parse_turntable
-puts parse_mp3_skull(search_string)
+#puts parse_mp3_skull(search_string)
 #download_to_path(parse_mp3_skull(search_string))
 
 
 
 
 # TODO
+# - automatically start playing song after download
+# - output download info to log file
+# - output activity to stdout
+# - add support for download progress viewer
 # - recover from errors and download next song in array
 # - accept argument for download path using ARGV
-# - accept argument for reading list of songs from file
+# - accept argument for search string
 # - add threads for concurrent downloads?
 # - add songs to iTunes playlist
+
+
+#file_to_open = "/path/to/file.txt"
+#system %{open "#{file_to_open}"}
